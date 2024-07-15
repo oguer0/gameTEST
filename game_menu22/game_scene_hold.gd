e143bc4414b0696f42b89391940a8a27acde6cba
oscarv157 <134034877+oscarv157@users.noreply.github.com>
2024-07-15 13:26:23 -0400

    simplified the player
    made the player appear in game scene

--- game_menu22/game_scene_hold.gd
+++ game_menu22/game_scene_hold.gd
@@ -4,12 +4,17 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$CenterContainer2/BackButton.grab_focus()
+	new_game()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
+	
+func new_game():
+	#score = 0
+	$Player.start($StartPosition.position)
+	#$StartTimer.start()
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://mainMenu.tscn")
